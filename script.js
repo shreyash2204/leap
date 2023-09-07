85cacc3fdc27b423156e915a222974004de85a4c
SHREYASH GUPTA <101275851+shreyash2204@users.noreply.github.com>
2023-09-07 16:02:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 30);
+})
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    //Toggle Nav
+    burger.addEventListener('click', ()=>{
+      nav.classList.toggle('nav-active');
+      
+      //Animate Links
+      navLinks.forEach((link, index)=>{
+        if(link.style.animation){
+          link.style.animation = ''
+        }else{
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+  
+        }
+      });
+      
+      //burger animation
+      burger.classList.toggle('toggle');
+      
+      
+    });    
+  }
+navSlide();
